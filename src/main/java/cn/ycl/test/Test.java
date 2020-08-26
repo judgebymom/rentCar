8e5ea8f00610cc33425fe839cd253bb91f3e5866
ycl <3020651945@qq.com>
2020-08-26 20:53:59 +0800

    的更好的规划

---
+++ src/main/java/cn/ycl/test/Test.java
@@ -0,0 +1,8 @@
+package cn.ycl.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
---
+++ src/main/java/cn/ycl/test/Test2.java
@@ -0,0 +1,4 @@
+package cn.ycl.test;
+
+public class Test2 {
+}
